f4069d5326d8963d4c89fe315d057c2ed4fbbf5f
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-05-24 11:29:27 -0400

    fixes UnboundLocalError and TypeError

--- webscraper.py
+++ webscraper.py
@@ -5,45 +5,54 @@ import requests
 app = Flask(__name__)
 
 def scrapeGame(game_id):
-  url = f'https://j-archive.com/showgame.php?game_id={game_id}'
-  pageToScrape = requests.get(url)
-  soup = BeautifulSoup(pageToScrape.text, "html.parser")
+    url = f'https://j-archive.com/showgame.php?game_id={game_id}'
+    pageToScrape = requests.get(url)
+    
+    if pageToScrape.status_code != 200:
+        return {'error': 'Game not found or unable to retrieve the page'}
 
+    soup = BeautifulSoup(pageToScrape.text, "html.parser")
 
-  game_title = soup.find('div', attrs={'id':'game_title'}).contents[0].text.strip()
-  game_comments = soup.find('div', attrs={'id':'game_comments'}).text.strip()
-  categories = [cat.text.strip() for cat in soup.findAll('td', attrs={'class':'category_name'})]
-  category_comments = [com.text.strip() for com in soup.findAll('td', attrs={'class':'category_comments'})]
+    game_title = soup.find('div', attrs={'id': 'game_title'})
+    game_title_text = game_title.get_text(strip=True) if game_title else "Title not found"
 
-  jeopardy_clues = []
-  jeopardy_responses = []
-  double_jeopardy_clues = []
-  double_jeopardy_responses = []
+    game_comments = soup.find('div', attrs={'id': 'game_comments'})
+    game_comments_text = game_comments.get_text(strip=True) if game_comments else "Comments not found"
 
-  for y in range(1,6):
-    for x in range(1,7):
-      clue = soup.find('td', attrs={'id': f'clue_J_{x}_{y}'})
-      if clue:
-        jeopardy_clues.append(clue.text.strip())
+    categories = [cat.get_text(strip=True) for cat in soup.findAll('td', attrs={'class': 'category_name'})]
+    category_comments = [com.get_text(strip=True) for com in soup.findAll('td', attrs={'class': 'category_comments'})]
 
-      double_clue = soup.find('td', attrs={'id': f'clue_DJ_{x}_{y}'})
-      if double_clue:
-        double_jeopardy_clues.append(double_clue.text.strip())
-  final_jeopardy_clue = soup.find('td', attrs={'id':'clue_FJ'})
-  final_jeopardy_clue_text = final_jeopardy_clue.text.strip() if final_jeopardy_clue else ""
+    jeopardy_clues = []
+    jeopardy_responses = []
+    double_jeopardy_clues = []
+    double_jeopardy_responses = []
 
-  responses = soup.findAll('em', attrs={'class':'correct_response'})
-  for count, response in enumerate(responses, start=1):
-    if count < 31:
-      jeopardy_responses.append(response.text.strip())
-    elif 30 < count < 61:
-      double_jeopardy_responses.append(response.text.strip())
-    else:
-      final_jeopardy_response = response.text.strip()
+    for y in range(1, 6):
+        for x in range(1, 7):
+            clue = soup.find('td', attrs={'id': f'clue_J_{x}_{y}'})
+            if clue:
+                jeopardy_clues.append(clue.get_text(strip=True))
 
-  return {
-        'game_title': game_title,
-        'game_comments': game_comments,
+            double_clue = soup.find('td', attrs={'id': f'clue_DJ_{x}_{y}'})
+            if double_clue:
+                double_jeopardy_clues.append(double_clue.get_text(strip=True))
+
+    final_jeopardy_clue = soup.find('td', attrs={'id': 'clue_FJ'})
+    final_jeopardy_clue_text = final_jeopardy_clue.get_text(strip=True) if final_jeopardy_clue else "Final Jeopardy clue not found"
+
+    final_jeopardy_response = "Final Jeopardy response not found"
+    responses = soup.findAll('em', attrs={'class': 'correct_response'})
+    for count, response in enumerate(responses, start=1):
+        if count < 31:
+            jeopardy_responses.append(response.get_text(strip=True))
+        elif 30 < count < 61:
+            double_jeopardy_responses.append(response.get_text(strip=True))
+        elif count == 61:
+            final_jeopardy_response = response.get_text(strip=True)
+
+    return {
+        'game_title': game_title_text,
+        'game_comments': game_comments_text,
         'categories': categories,
         'category_comments': category_comments,
         'jeopardy_round': {
