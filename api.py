c2dcba55978d54a7f8bf4278e491ff83307535ad
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-05-26 11:48:10 -0400

    adds pagination

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -8,7 +8,23 @@ with open('jeopardy_games.json', 'r') as f:
 
 @app.route('/api/games', methods=['GET'])
 def get_all_games():
-    return jsonify(games_data)
+    page = request.args.get('page', default=1, type=int)
+    size = request.args.get('size', default=10, type=int)
+
+    start = (page - 1) * size
+    end = start + size
+
+    paginated_games = games_data[start:end]
+
+    response = {
+        'page': page,
+        'size': size,
+        'total_games': len(games_data),
+        'total_pages': (len(games_data) + size - 1) // size,  # Calculate total pages
+        'games': paginated_games
+    }
+
+    return jsonify(response)
 
 @app.route('/api/games/<int:game_id>', methods=['GET'])
 def get_game_by_id(game_id):
